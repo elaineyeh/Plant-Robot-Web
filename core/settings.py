72aedf36923ff55f767640e3ebad3ad43dd7c9fb
elaineyeh <10456018@ntub.edu.tw>
2019-10-10 23:19:05 +0800

    Fix collect static files

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = env('STATIC_URL', default='/static/')
 
@@ -150,13 +150,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
-STATICFILES_DIRS = (
-    ("font", os.path.join(STATIC_ROOT, 'font')),
-    ("js", os.path.join(STATIC_ROOT, 'js')),
-    ("css", os.path.join(STATIC_ROOT, 'css')),
-    ("images", os.path.join(STATIC_ROOT, 'images')),
-)
 
 if env.bool('GCP', default=False):
     DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
