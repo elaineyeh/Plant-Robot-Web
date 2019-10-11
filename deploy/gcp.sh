9fb123b19ba4ab9913305fc467db2b6695f435a0
elaineyeh <10456018@ntub.edu.tw>
2019-10-11 13:10:33 +0800

    Fix deploy

--- deploy/gcp.sh
+++ deploy/gcp.sh
@@ -1,3 +1,5 @@
 cd ~/Plant-Robot-Web
 git pull
+cat key.json | gcloud auth activate-service-account --key-file=-
+docker login -u oauth2accesstoken -p "$(gcloud auth application-default print-access-token)" https://asia.gcr.io
 docker-compose pull && docker-compose up -d --no-deps
